9ceec441f67ef92e6c4d5d37fe58d01946911691
Teena Verma <teena21072004@gmail.com>
2026-07-19 16:32:18 +0530

    feat: change repository route

--- app/routes/repository.py
+++ app/routes/repository.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
-from app.services.pipeline_service import generate_repository_documentation
+
 from app.services.repository_service import extract_zip_file, save_repository_zip,analyze_project
 
 router = APIRouter(
@@ -29,17 +29,3 @@ async def repository_router(file: UploadFile = File(...)):
     "analysis": analysis_results
 }
     
-    
-@router.post("/generate-documentation")
-
-async def generate_repository_docs_route(
-            file: UploadFile = File(...),):
-            
-            if not file.filename.endswith(".zip"):
-                raise HTTPException(status_code=400, detail="Invalid file format. Please upload a ZIP file.")
-            if not file.filename:
-                raise HTTPException(status_code=400,             detail="Uploaded file must have a filename",
-                    )
-            saved_file_path = save_repository_zip(file)
-            
-            return generate_repository_documentation(saved_file_path)
