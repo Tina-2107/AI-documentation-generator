82c7b72e3a4a3def86da76d7404b4bbf021452b1
Teena Verma <teena21072004@gmail.com>
2026-07-19 16:30:28 +0530

    feat: add documentation pipeline

---
+++ app/routes/pipeline.py
@@ -0,0 +1,22 @@
+from app.services.repository_service import save_repository_zip
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from app.services.pipeline_service import generate_repository_documentation
+
+router = APIRouter(
+    prefix="/repositories",
+    tags=["Repositories"],)
+
+    
+@router.post("/generate-documentation")
+
+async def generate_repository_docs_route(
+            file: UploadFile = File(...),):
+            
+            if not file.filename.endswith(".zip"):
+                raise HTTPException(status_code=400, detail="Invalid file format. Please upload a ZIP file.")
+            if not file.filename:
+                raise HTTPException(status_code=400,             detail="Uploaded file must have a filename",
+                    )
+            saved_file_path = save_repository_zip(file)
+            
+            return generate_repository_documentation(saved_file_path)
